5f3027af3c42b93b865f3427e515bf5cd34f37dc
Beau <beau@beaumartinez.com>
2014-12-19 23:07:12 +0100

    Refactor

--- twittercide.py
+++ twittercide.py
@@ -34,7 +34,16 @@ class Twittercider(object):
 
         self._get_or_create_parent_dir()
 
-    def _prepare_post_multipart_related(self, url, files):
+    def _get_or_create_parent_dir(self):
+        metadata = {
+            'title': 'Twittercide',
+            'mimeType': 'application/vnd.google-apps.folder',
+        }
+        parent_dir = self._get_or_upload(metadata)
+
+        self.parent_dir_id = parent_dir['id']
+
+    def _prepare_upload(self, url, files):
         request = Request('post', url, files=files)
         request = self.session.prepare_request(request)
 
@@ -45,43 +54,11 @@ class Twittercider(object):
 
         return request
 
-    def _prepare_upload(self, title, file_):
-        '''Prepares a Request to upload the file <file_>, giving it the title
-        <title>.
-
-        '''
-        # Google Drive expects multipart/related, and for the fields to be in a
-        # particular order. First, the metadata, then the file.
-        #
-        # It expects the metadata as application/json, and the file as base64 (with
-        # Content-Transfer-Encoding: base64).
-
-        metadata = {
-            'title': title,
-            'parents': (self.parent_dir_id,)
-        }
-
-        files = OrderedDict()
-        files['metadata'] = ('metadata', json.dumps(metadata), 'application/json')
-        files['file'] = ('file', b64encode(file_.read()), 'image/png', {'Content-Transfer-Encoding': 'base64'})
-
-        request = self._prepare_post_multipart_related('https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart', files)
-
-        return request
-
-    def _get_or_create_parent_dir(self):
-        metadata = {
-            'title': 'Twittercide',
-            'mimeType': 'application/vnd.google-apps.folder',
-        }
-        self.parent_dir = self._get_or_upload(metadata)
-        self.parent_dir_id = self.parent_dir['id']
-
-    def _get_or_upload(self, metadata, file_=None):
+    def _prepare_query(self, metadata):
         query = list()
 
         for key, value in metadata.iteritems():
-            if key in ('parents',):
+            if key == 'parents':
                 for subvalue in value:
                     # When in the metadata, each parent item is object. To search, we just need the ID
                     subvalue = subvalue['id']
@@ -92,13 +69,16 @@ class Twittercider(object):
 
         query = ' and '.join(query)
 
-        log.debug('_get_or_upload query {}'.format(query))
+        return query
+
+    def _get_or_upload(self, metadata, file_=None):
+        query = self._prepare_query(metadata)
+        log.debug('_get_or_upload: query {}'.format(query))
 
         response = self.session.get('https://www.googleapis.com/drive/v2/files', params={
             'q': query,
         })
-
-        log.debug('_get_or_upload get response {}'.format(response.text))
+        log.debug('_get_or_upload: get response {}'.format(response.text))
 
         response_data = response.json()
         results = response_data['items']
@@ -106,39 +86,47 @@ class Twittercider(object):
         try:
             result = results[0]
         except IndexError:
-            log.debug('_get_or_upload get not found. Uploading.'.format(response.text))
+            log.debug('_get_or_upload: get not found. Uploading.'.format(response.text))
+
+            # Google Drive expects multipart/related, and for the fields to be in a
+            # particular order. First, the metadata, then the file.
+            #
+            # It expects the metadata as application/json, and the file as base64 (with
+            # Content-Transfer-Encoding: base64).
+
+            url = 'https://www.googleapis.com/upload/drive/v2/files'
 
             files = OrderedDict()
             files['metadata'] = ('metadata', json.dumps(metadata), 'application/json')
 
-            url = 'https://www.googleapis.com/upload/drive/v2/files'
-
             if file_:
-                files['file'] = ('file', b64encode(file_.read()), 'image/png', {'Content-Transfer-Encoding': 'base64'})
                 url = 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart'
+                files['file'] = ('file', b64encode(file_.read()), 'image/png', {'Content-Transfer-Encoding': 'base64'})
 
-            request = self._prepare_post_multipart_related(url, files)
+            request = self._prepare_upload(url, files)
             response = self.session.send(request)
 
-            log.debug('_get_or_upload upload response {}'.format(response.text))
+            log.debug('_get_or_upload: upload response {}'.format(response.text))
 
             response_data = response.json()
             result = response_data
 
         return result
 
-    def _upload(self, url):
+    def _backup_twitter_media(self, url):
         title = basename(url)
+        metadata = {
+            'title': title,
+            'parents': ({
+                'kind': 'drive#file',
+                'id': self.parent_dir_id,
+            },)
+        }
 
         file_response = self.session.get(url)
-
         file_ = file_response.content
         file_ = StringIO(file_)
 
-        metadata = {
-            'title': title,
-            'parents': (self.parent_dir,)
-        }
         self._get_or_upload(metadata, file_)
 
     def _get_tweets(self):
@@ -153,7 +141,7 @@ class Twittercider(object):
             if 'extended_entities' in tweet:
                 if 'media' in tweet['extended_entities']:
                     for media in tweet['extended_entities']['media']:
-                        self._upload(media['media_url'])
+                        self._backup_twitter_media(media['media_url'])
 
 
 if __name__ == '__main__':
