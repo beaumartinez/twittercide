dab7406b132a79af104275d6c5e268c91e100ec9
Beau <beau@beaumartinez.com>
2014-12-19 15:54:11 +0100

    Add parent dir logic
    Get or create, and use that in the example.

--- twittercide.py
+++ twittercide.py
@@ -3,6 +3,7 @@
 from argparse import ArgumentParser
 from base64 import b64encode
 from collections import OrderedDict
+from urllib import quote_plus
 import json
 import logging
 
@@ -28,7 +29,7 @@ session = Session()
 session.mount('https://', Foauth(args.email, args.password))
 
 
-def _prepare_upload(session, title, file_):
+def _prepare_upload(session, parent_dir, title, file_):
     '''Prepares a Request to upload the file <file_>, giving it the title
     <title>, using an existing session <session>.
 
@@ -41,13 +42,20 @@ def _prepare_upload(session, title, file_):
 
     metadata = {
         'title': title,
+        'parents': (parent_dir,)
     }
 
     files = OrderedDict()
     files['metadata'] = ('metadata', json.dumps(metadata), 'application/json')
     files['file'] = ('file', b64encode(file_.read()), 'image/png', {'Content-Transfer-Encoding': 'base64'})
 
-    request = Request('post', 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart', files=files)
+    request = _prepare_post_multipart_related('https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart', files)
+
+    return request
+
+
+def _prepare_post_multipart_related(url, files):
+    request = Request('post', url, files=files)
     request = session.prepare_request(request)
 
     # This is a bit ugly (ideally requests would expose a way to set the
@@ -58,11 +66,45 @@ def _prepare_upload(session, title, file_):
     return request
 
 
+def _get_or_create_parent_dir(session):
+    query = 'title = "Twittercide" and mimeType = "application/vnd.google-apps.folder"'
+
+    response = session.get('https://www.googleapis.com/drive/v2/files', params={
+        'q': query,
+    })
+
+    log.debug('Parent dir search response {}'.format(response.text))
+
+    response_data = response.json()
+    results = response_data['items']
+
+    try:
+        parent_dir = results[0]
+    except IndexError:
+        metadata = {
+            'title': 'Twittercide',
+            'mimeType': 'application/vnd.google-apps.folder',
+        }
+
+        files = OrderedDict()
+        files['metadata'] = ('metadata', json.dumps(metadata), 'application/json')
+
+        request = _prepare_post_multipart_related('https://www.googleapis.com/upload/drive/v2/files', files)
+        response = session.send(request)
+
+        log.debug('Parent dir create response {}'.format(response.text))
+
+        response_data = response.json()
+        parent_dir = response_data
+
+    return parent_dir
+
+
 if __name__ == '__main__':
-    # TODO: Search first
+    parent_dir = _get_or_create_parent_dir()
 
     with open('/Users/beau/Pictures/1418760457552.png', mode='rb') as file_:
-        request = _prepare_upload(session, 'lel.png', file_)
+        request = _prepare_upload(session, parent_dir, 'lel.png', file_)
 
     response = session.send(request)
 
